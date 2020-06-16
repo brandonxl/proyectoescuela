5d88454a9cfdcd5bc1039ac69d31586151026100
Brandon Cortes <bransmiguel13@gmail.com>
2020-06-16 00:22:00 -0500

    se hizo una modificacion del 100% en el html css ademas de agregarse codigo JS

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+//lOGIN FALSO
+
+
+$(document).ready(function(){
+
+$(".contenedor form").submit(function(){
+    var form_name = $("#registro_name").val();
+
+    localStorage.setItem("form_name", form_name);
+});
+
+var form_name = localStorage.getItem("form_name");
+
+
+if(form_name != null && form_name!= "undefined"){
+ 
+    var insertar = $("#usuario p");
+
+    $("#usuario p").html("Bienvenido "+form_name);
+    insertar.append("<br/><a href='#' id='logout'>Cerrar Sesion</a> ")
+    $(".contenedor").hide();
+    $(".formulario").hide();
+    $("#posicion").hide();
+   
+   
+
+    $("#logout").click(function(){
+        localStorage.clear();
+        location.reload();
+    });
+}
+
+
+
+});
